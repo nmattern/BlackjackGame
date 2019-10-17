d3cbb064fa0f144510361d08fdff725abf523887
Storm Mattern <storm.mattern@gmail.com>
2019-10-17 17:31:44 -0500

    Added the necessary classes and implemented basic attributes for them

---
+++ BlackJackApplication/BlackJackApplication/Deck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackApplication
+{
+    class Deck
+    {
+        private List<Card> cardsInDeck = new List<Card>();
+
+        public List<Card> Cards
+        {
+            get { return cardsInDeck; }
+            set { cardsInDeck = value; }
+        }
+
+        public void addCardToDeck(Card card)
+        {
+            cardsInDeck.Add(card);
+        }
+
+        public void shuffleCards()
+        {
+
+        }
+    }
+}
---
+++ BlackJackApplication/BlackJackApplication/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJackApplication
+{
+    class Player
+    {
+        private int currentValueOfHand;
+        private int currentAmountofMoney;
+        enum playerChoiceValues
+        {
+            Stand = 1,
+            Hit = 2,
+            Reset = 3
+        }
+        public int ValueOfHand
+        {
+            get { return currentValueOfHand; }
+            set { currentValueOfHand = value; }
+        }
+
+        public int AmountOfMoney
+        {
+            get { return currentAmountofMoney; }
+            set { currentAmountofMoney = value; }
+        }
+
+        public int playerChoice()
+        {
+            // return a value (possible enum) for player action
+            return ((int)playerChoiceValues.Stand);
+        }
+    }
+}
